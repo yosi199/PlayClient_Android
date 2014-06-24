66f83b0f60db9e835f5068aa462e44f85122e1c1
Yosi <yosimizrachi7@gmail.com>
2014-06-24 19:30:44 +0300

    Implemented shuffle messages

---
+++ java/Network/NetworkService.java
@@ -0,0 +1,22 @@
+package Network;
+
+import android.app.Service;
+
+/**
+ * Created by Unknown1 on 6/24/14.
+ */
+public class NetworkService extends Service {
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+}
